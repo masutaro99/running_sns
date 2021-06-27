a9aec90ab79d9d8d24caa088ba4d1d5717a42929
masutaro99 <maskenpa1001@gmail.com>
2021-06-28 00:32:11 +0900

    corsのorigin設定をすべてにしてみる12

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3001', 'https://www.masutaro99.com', 'https://masutaro99.com']
+    # origins ['http://localhost:3001', 'https://www.masutaro99.com', 'https://masutaro99.com']
+    origins "*"
 
     resource '*',
       headers: :any,
