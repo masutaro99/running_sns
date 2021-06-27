3c6773d0af5f5c23437e53eb66bec014c0382184
masutaro99 <maskenpa1001@gmail.com>
2021-06-27 22:52:21 +0900

    fix: rootページをparacticeのindexページにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'practices/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root "practices#index"
+  root "practices#index"
   get  "presignedurl"  => "presignedurl#index"
   resources :imgs, only: %i[index show create update destroy]
   resources :practices, only: %i[index show create update destroy]
